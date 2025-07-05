4b9b9804eaeb87bb5e4bf587aefb04db348581bc
Harsh <harshkmr.8996@gmail.com>
2025-07-05 18:00:12 +0530

    check the age for vote

---
+++ vote.py
@@ -0,0 +1,5 @@
+age = int(input("Apni umar bataiye:")) # user se input lene ke liye
+if age >= 18: # agar umar 18 se jyada ho ya barabar ho
+    print("aap vote de sakte hai") # user ko btane ke liye
+else: # agar umar 18 se km ho 
+    print("aap vote nahi de sakte hai")
